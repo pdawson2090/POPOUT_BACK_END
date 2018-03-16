b5e487b2030ab552498519239825cbbb733e1105
pdawson2090 <pedawso1@millersville.edu>
2018-03-16 12:13:40 -0400

    origins

--- src/main/java/popout/back/controller/ControllerApplication.java
+++ src/main/java/popout/back/controller/ControllerApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class ControllerApplication {
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class ControllerApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControllerApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+
+
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**");
+    }
 }
