839d855249033509f32f8c5f4ab3814560ef87e4
pdawson2090 <36117746+pdawson2090@users.noreply.github.com>
2018-03-16 01:31:54 -0400

    login validation and password encryption

--- Popout_Controller/src/main/java/popout/back/Services/Validation.java
+++ Popout_Controller/src/main/java/popout/back/Services/Validation.java
@@ -1,26 +1,33 @@
 package popout.back.Services;
 
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import popout.back.models.User;
+
 public class Validation
 {
     private static Argon2 argon2 = Argon2Factory.create();
+    public void argon2Secure(User user){
 
+        user.setPassword(argon2.hash(14,65336,1,user.getPassword()));
 
-    public boolean logins(String username, String pass)
+    }
+    public boolean loginValidation(String username, String pass)
     {
-
-        user = eDAO.getEmployeeByUsername(username);
+        User user = new User();
         try
         {
-            if (user.getUserName().equals(username))
+            if (user.getUsername().equals(username))
             {
 
                 // Verify password
-                if (argon2.verify(user.getPassword(),pass))
+                if (argon2.verify(user.getPassword(), pass))
                 {
                     System.out.println("Login Success!");
                     return true;
-                }else{
+                } else
+                {
                     System.out.println("Invalid Login");
                 }
 
