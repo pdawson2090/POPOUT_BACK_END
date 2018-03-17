e8d55da924e3b6a1e38bb9243a7d4b02866f083a
pdawson2090 <pedawso1@millersville.edu>
2018-03-17 19:22:37 -0400

    fixes

--- src/main/java/popout/back/controller/FrontController.java
+++ src/main/java/popout/back/controller/FrontController.java
@@ -1,6 +1,7 @@
 package popout.back.controller;
 
 import org.springframework.web.bind.annotation.*;
+import popout.back.models.Event;
 import popout.back.models.User;
 //import popout.back.models.Event;
 //import popout.back.models.User;
@@ -12,25 +13,25 @@ public class FrontController
 {
 
     @PostMapping("/login")
-    public boolean processRegistration (@RequestBody User user){
+    public boolean processLogin (@RequestBody User user){
         System.out.println("username: " + user.getUsername());
         System.out.println("password: " + user.getPassword());
         return true;
     }
+    @PostMapping("/register")
+    boolean processRegistration (@RequestBody User user){
+        return true;
+    }
+    @PostMapping("/event")
+    boolean processEventRegistration (@RequestBody Event event){
+        return true;
+    }
+    @PostMapping("/allevents")
+    List<Event> processEventRoster (){
+        //Get List Of All Events
+        return null;
+    }
 
-//    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-//    boolean processRegistration (@RequestBody User user){
-//        return true;
-//    }
-//    @RequestMapping(value = "/eventregistration", method = RequestMethod.POST)
-//    boolean processEventRegistration (@RequestBody Event event){
-//        return true;
-//    }
-//    @RequestMapping(value = "/eventRoster", method = RequestMethod.POST)
-//    List<Event> processEventRoster (){
-//        //Get List Of All Events
-//        return null;
-//    }
 
 
 
